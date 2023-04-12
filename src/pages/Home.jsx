f7fa7e0a26c62a8a89318183d164e8e7134aad2c
esg-gforsythe <84102314+esg-gforsythe@users.noreply.github.com>
2023-04-12 13:46:42 -0700

    async and await a fetch call to the tv Maze API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react"
 
 
 function Home() {
+const [searchString, setSearchString] =  useState('');
+
+const onSearchInputChange = (ev) => {
+  setSearchString(ev.target.value);
+}
+
+const onSearch = async (ev) => {
+ev.preventDefault();
+
+const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchString}`)
+const body = await response.json()
+console.log(body);
+
+
+
+}
   return (
-    <div>Home </div>
+    <div>
+      <form  onSubmit={onSearch}>
+      <input type="text" value={searchString} onChange={onSearchInputChange}/>  
+      <button type="submit" >Search</button>
+
+      </form>
+      
+    </div>
   )
 }
 
