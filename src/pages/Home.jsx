a128f266abd2caab7bb0546f40b91ca3110ddcd1
esg-gforsythe <84102314+esg-gforsythe@users.noreply.github.com>
2023-04-12 15:17:19 -0700

    refined API and created a helper function with error handling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,53 @@
-import { useState } from "react"
-
+import { useState } from 'react';
+import { searchForShows } from '../api/tvMaze';
 
 function Home() {
-const [searchString, setSearchString] =  useState('');
+  const [searchString, setSearchString] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
+  //holds the various changes to the input field
+  const onSearchInputChange = ev => {
+    setSearchString(ev.target.value);
+  };
+  //submits the form request from the button
+  const onSearch = async ev => {
+    ev.preventDefault();
 
-const onSearchInputChange = (ev) => {
-  setSearchString(ev.target.value);
-}
+    try {
+      setApiDataError(null);
+      const result = await searchForShows(searchString);
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
+  };
 
-const onSearch = async (ev) => {
-ev.preventDefault();
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error Occured: {apiDataError.message}</div>;
+    }
 
-const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchString}`)
-const body = await response.json()
-console.log(body);
+    if (apiData) {
+      return apiData.map(data => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+    return null;
+  };
 
-
-
-}
   return (
     <div>
-      <form  onSubmit={onSearch}>
-      <input type="text" value={searchString} onChange={onSearchInputChange}/>  
-      <button type="submit" >Search</button>
-
+      <form onSubmit={onSearch}>
+        <input
+          type="text"
+          value={searchString}
+          onChange={onSearchInputChange}
+        />
+        <button type="submit">Search</button>
       </form>
-      
+      <div>{renderApiData()}</div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
