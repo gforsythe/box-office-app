e586959e90a3d4969533e17d2c19a925935406e1
esg-gforsythe <84102314+esg-gforsythe@users.noreply.github.com>
2023-06-06 16:04:37 -0700

    swapped out useEffect with React Query Library to learn about best practice for data feteching and avoiding double-fetch

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,12 +1,17 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import {useQuery,} from '@tanstack/react-query'
 import { useParams } from "react-router-dom";
 import { getShowById } from "../api/tvMaze";
-
+//Custom Hook
+/*
+For Data Fetching try to not use useEffect. Better way is to use a library.
 const useShowById = (showId) => {
+  //Using React Hooks
   const [showData, setShowData] = useState(null);
   const [showError, setShowError] = useState(null)
   useEffect(() =>{
     // getShowById()
+    //with custom logic = custom hook
     async function fetchData(){
       try {
         const response = await getShowById(showId);
@@ -22,12 +27,19 @@ const useShowById = (showId) => {
 
   }, [showId])
   return {showData, showError}
-}
+}*/
 
 function Show() {
   const {showId} = useParams();
-  const {showData, showError} = useShowById(showId);
+  //calling the custom hook
+  // const {showData, showError} = useShowById(showId);
   
+  //Query
+  const {data: showData, error: showError} = useQuery({
+    queryKey:['show', showId], 
+    queryFn:() => getShowById(showId)
+  });
+
 
   if(showError){
     return <div>We have an error: {showError.message}</div>
