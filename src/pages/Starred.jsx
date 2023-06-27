04afe08bbd8522d0007bd8164da54177ca38543e
esg-gforsythe <84102314+esg-gforsythe@users.noreply.github.com>
2023-06-27 16:59:22 -0700

    final part of the starred page. added a new function to fetch shows and ccreated a promise to resolve all while usingn use query and applied conditional rendering for the starred page

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,10 +1,33 @@
+import { getShowsByIds } from "../api/tvMaze";
+import ShowGrid from "../components/shows/ShowGrid";
 import { useStarredShows } from "../lib/useStarredShows";
+import { useQuery } from "@tanstack/react-query";
 
 function Starred() {
-  const [starredShows] = useStarredShows()
+  const [starredShowsIds] = useStarredShows();
 
 
-  return <div>Starred Page, starred {starredShows.length}</div>;
+  const {data: starredShows, error:starredShowsError} = useQuery({
+    queryKey:['starred', starredShowsIds ],
+    queryFn:() => getShowsByIds(starredShowsIds).then(result => result.map(show => ({show}))),
+    refetchOnWindowFocus: false,
+  })
+
+
+if(starredShows?.length === 0 ){
+  return <div>No shows are starred</div>
+} 
+
+if(starredShows?.length > 0){
+  return <ShowGrid shows={starredShows}/>
+} 
+ 
+
+if (starredShowsError){
+  return <div>There was an error: {starredShowsError.message}</div>
+}
+
+  return <div>Shows are still loading...</div>
 }
 
 export default Starred;
