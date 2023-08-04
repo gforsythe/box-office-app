9d8de3ace4378cc747e896762bd94dd09c3a0469
esg-gforsythe <84102314+esg-gforsythe@users.noreply.github.com>
2023-08-04 16:04:55 -0700

    added meta descript and service worker to be PWA

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from  './serviceWorkerRegistration';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,12 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if(process.env.NODE_ENV === 'production'){
+
+  serviceWorkerRegistration.register()
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
